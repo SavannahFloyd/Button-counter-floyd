017d1956f42e9b1e6e4c27b073c012e82dc36972
Savannah <floydsm1@g.cofc.edu>
2017-02-06 21:03:52 -0500

    commit

--- app/src/main/java/com/example/savannah/button_counter_floyd/ButtonCounter.java
+++ app/src/main/java/com/example/savannah/button_counter_floyd/ButtonCounter.java
@@ -1,21 +1,71 @@
 package com.example.savannah.button_counter_floyd;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.lang.Object;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 
 public class ButtonCounter extends AppCompatActivity {
+    private Button ascendingButton;
+    private Button descendingButton;
+    private Button clearButton;
+    private TextView countText;
+    private Integer counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_counter);
-        final Button button = (Button) findViewById(R.id.button_id);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        counter = 0;
+        ascendingButton = (Button) findViewById(R.id.ascend_button);
+        descendingButton = (Button) findViewById(R.id.descend_button);
+        clearButton = (Button) findViewById(R.id.clear_button);
+        countText = (TextView) findViewById(R.id.countText);
+        countText.setText(counter.toString());
+
+        ascendingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
+                add();
+                countText.setText(counter.toString());
             }
         });
+        descendingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subtract();
+                countText.setText(counter.toString());
+            }
+        });
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clear();
+                countText.setText(counter.toString());
+            }
+        });
+    }
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("countSave", counter);
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        counter = savedInstanceState.getInt("countSave");
+        countText.setText(counter.toString());
+    }
+
+    public void add() {
+        counter = counter + 1;
+    }
+
+    public void subtract() {
+        counter = counter - 1;
+    }
+
+    public void clear() {
+        counter = 0;
     }
 }
